01a588bcd348599915ebaa76a23ffb2d39c3f1db
Snakeonex <sheeproman@gmail.com>
2024-11-25 17:02:34 +0100

    custom patch proj option + start of titok paper

--- train_titok.py
+++ train_titok.py
@@ -4,48 +4,54 @@ from einops import rearrange, repeat
 from dataclasses import dataclass
 
 from transformer import Transformer, TransformerConfig
+from train_vit import ViTConfig, ViT
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-@dataclass
-class ViTConfig:
-    image_size: int = 256
-    patch_size: int = 16
-    n_layers: int = 12
-    n_heads: int = 12
-    n_embd: int = 768
-    extra_tokens: int = 1
-    dropout: float = 0.25
-
-    def __post_init__(self):
-        self.n_patches = (self.image_size // self.patch_size) ** 2
-        self.patch_dim = 3 * self.patch_size ** 2
-
-class ViT(nn.Module):
-    def __init__(self, args: ViTConfig, _patch_proj=None):
-        super(ViT, self).__init__()
-        self.config = args
-        if _patch_proj is not None: self.patch_proj = _patch_proj
-        else: self.patch_proj = nn.Conv2d(in_channels=3, out_channels=args.n_embd, kernel_size=args.patch_size, stride=args.patch_size)
-        self.pos_emb = nn.Embedding(args.n_patches, args.n_embd)
-        self.extra_emb = nn.Embedding(args.extra_tokens, args.n_embd)
-        self.transformer = Transformer(TransformerConfig(args.n_layers, args.n_heads, args.n_embd, args.n_patches + args.extra_tokens, args.dropout))
-
-    def forward(self, x):
-        patch_emb = self.patch_proj(x)
-        patch_emb = rearrange(patch_emb, 'b c h w -> b (h w) c')
-        patch_emb = patch_emb + self.pos_emb(torch.arange(self.config.n_patches, device=patch_emb.device))
-
-        extra_emb = repeat(self.extra_emb.weight, 'n d -> b n d', b=x.shape[0])
-        emb = torch.cat([extra_emb, patch_emb], dim=1)
-        return self.transformer(emb)
-
 class ViTClassifier(nn.Module):
-    def __init__(self, vit_config: ViTConfig, num_classes=1000):
+    def __init__(self, vit_config: ViTConfig):
         super(ViTClassifier, self).__init__()
         self.vit = ViT(vit_config)
-        self.head = nn.Linear(vit_config.n_embd, num_classes)
+        self.head = nn.Linear(vit_config.n_embd, vit_config.num_classes)
     def forward(self, x):
         return self.head(self.vit(x)[:,0])
+
+@dataclass
+class TiTokConfig:
+    image_sz = 128
+    latent_tokens: int = 32
+    codebook_size: int = 512
+    latent_dim: int = 256
+
+# image(3, 128, 128) -> TiTokEncoder(128/16^2, n_embed) -> latent(latent_tokens, latent_dim) -> Quantizer(latent_tokens, latent_dim) -> TiTokDecoder(128/16^2, n_embed) -> image(3, 128, 128)
+
+class TiTokEncoder(nn.Module):
+    def __init__(self, titok_config: TiTokConfig):
+        super(TiTokEncoder, self).__init__()
+        self.latent_tokens = titok_config.latent_tokens
+        self.vit = ViT(ViTConfig(image_size=titok_config.image_sz, extra_tokens=titok_config.latent_tokens))
+    def forward(self, x):
+        out_embd = self.vit(x)
+        return out_embd
+
+class Quantizer(nn.Module):
+    def __init__(self, titok_config: TiTokConfig):
+        super(Quantizer, self).__init__()
+        self.codebook = nn.Embedding(titok_config.codebook_size, titok_config.latent_dim)
+    def forward(self, x):
+        pass
+
+class TiTokDecoder(nn.Module):
+    def __init__(self, titok_config: TiTokConfig):
+        super(TiTokDecoder, self).__init__()
+    def forward(self, x):
+        pass
+
+class TiTok(nn.Module):
+    def __init__(self, titok_config: TiTokConfig, vit_config: ViTConfig):
+        super(TiTok, self).__init__()
+        self.vit_encoder = ViT(vit_config, extra_tokens=titok_config.latent_tokens)
+    def forward(self, x):
+        pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -98,7 +104,7 @@ if __name__ == '__main__':
             loss.backward()
             optim.step()
             bar.set_description(f"e={epoch} loss={loss.item():.3f}")
-            if i % 10: wandb.log({"train/loss": loss.item()})
+            if i % 10 == 0: wandb.log({"train/loss": loss.item()})
         train_loss /= len(train_loader)
 
         with torch.no_grad():
