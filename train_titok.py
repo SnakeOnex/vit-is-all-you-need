c55174ad0b0f187cccf6d99bb09fd559657266cd
Snakeonex <sheeproman@gmail.com>
2024-11-27 09:44:48 +0100

    fixed quantizer

--- train_titok.py
+++ train_titok.py
@@ -30,90 +30,18 @@ class TiTokEncoder(nn.Module):
         print(latent_embd.shape)
         return latent_embd
 
-class VectorQuantizer(torch.nn.Module):
-    def __init__(self,
-                 codebook_size: int = 1024,
-                 token_size: int = 12,
-                 commitment_cost: float = 0.25,
-                 use_l2_norm: bool = True,
-                 ):
-        super().__init__()
-        self.commitment_cost = commitment_cost
-
-        self.embedding = torch.nn.Embedding(codebook_size, token_size)
-        self.embedding.weight.data.uniform_(-1.0 / codebook_size, 1.0 / codebook_size)
-        self.use_l2_norm = use_l2_norm
-
-    # Ensure quantization is performed using f32
-    # @autocast(enabled=False)
-    def forward(self, z: torch.Tensor) -> Tuple[torch.Tensor, Mapping[Text, torch.Tensor]]:
-        print(f"{z.shape=}")
-        z = z.float()
-        # z = rearrange(z, 'b c h w -> b h w c').contiguous()
-        z_flattened = rearrange(z, 'b h w c -> (b h w) c')
-
-        print(f"{z_flattened.shape=}")
-        print(f"{self.embedding.weight.shape=}")
-
-        if self.use_l2_norm:
-            z_flattened = torch.nn.functional.normalize(z_flattened, dim=-1)
-            embedding = torch.nn.functional.normalize(self.embedding.weight, dim=-1)
-        else:
-            embedding = self.embedding.weight
-        d = torch.sum(z_flattened**2, dim=1, keepdim=True) + \
-            torch.sum(embedding**2, dim=1) - 2 * \
-            torch.einsum('bd,dn->bn', z_flattened, embedding.T)
-
-        min_encoding_indices = torch.argmin(d, dim=1) # num_ele
-        z_quantized = self.get_codebook_entry(min_encoding_indices).view(z.shape)
-
-        if self.use_l2_norm:
-            z = torch.nn.functional.normalize(z, dim=-1)
-
-        # compute loss for embedding
-        commitment_loss = self.commitment_cost * torch.mean((z_quantized.detach() - z) **2)
-        codebook_loss = torch.mean((z_quantized - z.detach()) **2)
-
-        loss = commitment_loss + codebook_loss
-
-        # preserve gradients
-        z_quantized = z + (z_quantized - z).detach()
-
-        # reshape back to match original input shape
-        z_quantized = rearrange(z_quantized, 'b h w c -> b c h w').contiguous()
-
-        result_dict = dict(
-            quantizer_loss=loss,
-            commitment_loss=commitment_loss,
-            codebook_loss=codebook_loss,
-            min_encoding_indices=min_encoding_indices.view(z_quantized.shape[0], z_quantized.shape[2], z_quantized.shape[3])
-        )
-
-        return z_quantized, result_dict
-
-    def get_codebook_entry(self, indices):
-        if len(indices.shape) == 1:
-            z_quantized = self.embedding(indices)
-        elif len(indices.shape) == 2:
-            z_quantized = torch.einsum('bd,dn->bn', indices, self.embedding.weight)
-        else:
-            raise NotImplementedError
-        if self.use_l2_norm:
-            z_quantized = torch.nn.functional.normalize(z_quantized, dim=-1)
-        return z_quantized
-
 class Quantizer(nn.Module):
     def __init__(self, titok_config: TiTokConfig):
         super(Quantizer, self).__init__()
         self.codebook = nn.Embedding(titok_config.codebook_size, titok_config.latent_dim)
         self.codebook.weight.data.uniform_(-1.0 / titok_config.codebook_size, 1.0 / titok_config.codebook_size)
     def forward(self, x):
-        nearest_neighbors = torch.cdist(x, self.codebook.weight)
-        indices = nearest_neighbors.argmin(dim=-1)
-        quantized = x + (self.codebook(indices) - x).detach()
+        indices = torch.cdist(x, self.codebook.weight).argmin(dim=-1)
+        quantized = self.codebook(indices)
         codebook_loss = (quantized - x.detach()).pow(2).mean()
-        commitment_loss = (quantized.detach() - x).pow(2).mean()
+        commitment_loss = 0.25 * (quantized.detach() - x).pow(2).mean()
         quantize_loss = codebook_loss + commitment_loss
+        quantized = x + (quantized - x).detach() # copy gradients
         return quantized, indices, quantize_loss
 
 class TiTokDecoder(nn.Module):
@@ -181,16 +109,11 @@ if __name__ == '__main__':
         for i, (images, labels) in enumerate(bar):
             images, labels = images.to(device), labels.to(device)
             optim.zero_grad()
+
             latent_embs = titok_enc(images)[:,:titok_config.latent_tokens]
-            print(f"{latent_embs.shape=}")
-            # quantized, indices, quantize_loss = quantizer(latent_embs)
-            # print(f"{latent_embs.shape=}")
-            latent_embs_titok = rearrange(latent_embs, 'b c w -> b c 1 w')
-            quantized, result_dict = quantizer_titok(latent_embs_titok)
-            quantize_loss = result_dict["quantizer_loss"]
-            # print(f"{quantized.shape=}")
-            quantized = rearrange(quantized, 'b c h 1 -> b h c')
+            quantized, indices, quantize_loss = quantizer(latent_embs)
             image_recon = titok_dec(quantized)
+
             recon_loss = nn.functional.mse_loss(image_recon, images)
             loss = recon_loss + quantize_loss
             loss.backward()
