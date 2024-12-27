6d3b09b676fd424a7a40a2414eb0ccfc83e22890
Snakeonex <sheeproman@gmail.com>
2024-12-27 12:54:18 +0100

    enhancing vqgan code

---
+++ train_enhancing_vitvqgan.py
@@ -0,0 +1,352 @@
+import torch, torch.nn as nn, torchvision, lpips
+import argparse, tqdm, wandb, time
+from einops import rearrange
+from torch.amp import autocast, GradScaler
+from dataclasses import dataclass
+
+from utils import *
+from train_vit import ViTConfig, ViT
+from datasets import get_imagenet_loaders, get_dmlab_image_loaders, get_minecraft_image_loaders
+from einops.layers.torch import Rearrange
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.deterministic = False
+
+@dataclass
+class ViTVQGANConfig:
+    image_size: int
+    patch_size: int
+    codebook_size: int
+    latent_dim: int
+    transformer: str
+    def __post_init__(self):
+        self.patch_dim = self.image_size // self.patch_size
+        self.n_patches = self.patch_dim**2
+        self.latent_tokens = self.n_patches
+        self.enc_vit_config = ViTConfig(self.image_size, 3, self.patch_size, self.transformer, 0, 0.0)
+        self.n_embd = self.enc_vit_config.trans_config.n_embd
+        self.dec_vit_config = ViTConfig(self.latent_tokens, self.n_embd, 1, self.transformer, 0, 0.0)
+        self.dec_vit_config.n_patches = self.latent_tokens #HACK: is this line necessary?
+
+######################
+## ENHANCING LAYERS ##
+######################
+
+def get_2d_sincos_pos_embed(embed_dim, grid_size):
+    """
+    grid_size: int or (int, int) of the grid height and width
+    return:
+    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    """
+    grid_size = (grid_size, grid_size) if type(grid_size) != tuple else grid_size
+    grid_h = np.arange(grid_size[0], dtype=np.float32)
+    grid_w = np.arange(grid_size[1], dtype=np.float32)
+    grid = np.meshgrid(grid_w, grid_h)  # here w goes first
+    grid = np.stack(grid, axis=0)
+
+    grid = grid.reshape([2, 1, grid_size[0], grid_size[1]])
+    pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
+
+    return pos_embed
+
+
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 2 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = np.concatenate([emb_h, emb_w], axis=1) # (H*W, D)
+    return emb
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float)
+    omega /= embed_dim / 2.
+    omega = 1. / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum('m,d->md', pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out) # (M, D/2)
+    emb_cos = np.cos(out) # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        # we use xavier_uniform following official JAX ViT:
+        torch.nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.LayerNorm):
+        nn.init.constant_(m.bias, 0)
+        nn.init.constant_(m.weight, 1.0)
+    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        w = m.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim: int, fn: nn.Module) -> None:
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x: torch.FloatTensor, **kwargs) -> torch.FloatTensor:
+        return self.fn(self.norm(x), **kwargs)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim: int, hidden_dim: int) -> None:
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, dim)
+        )
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        return self.net(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim: int, heads: int = 8, dim_head: int = 64) -> None:
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Linear(inner_dim, dim) if project_out else nn.Identity()
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        attn = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+        attn = self.attend(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim: int, depth: int, heads: int, dim_head: int, mlp_dim: int) -> None:
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for idx in range(depth):
+            layer = nn.ModuleList([PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head)),
+                                   PreNorm(dim, FeedForward(dim, mlp_dim))])
+            self.layers.append(layer)
+        self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
+        return self.norm(x)
+
+
+class ViTEncoder(nn.Module):
+    def __init__(self, image_size: Union[Tuple[int, int], int], patch_size: Union[Tuple[int, int], int],
+                 dim: int, depth: int, heads: int, mlp_dim: int, channels: int = 3, dim_head: int = 64) -> None:
+        super().__init__()
+        image_height, image_width = image_size if isinstance(image_size, tuple) \
+                                    else (image_size, image_size)
+        patch_height, patch_width = patch_size if isinstance(patch_size, tuple) \
+                                    else (patch_size, patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        en_pos_embedding = get_2d_sincos_pos_embed(dim, (image_height // patch_height, image_width // patch_width))
+
+        self.num_patches = (image_height // patch_height) * (image_width // patch_width)
+        self.patch_dim = channels * patch_height * patch_width
+
+        self.to_patch_embedding = nn.Sequential(
+            nn.Conv2d(channels, dim, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b c h w -> b (h w) c'),
+        )
+        self.en_pos_embedding = nn.Parameter(torch.from_numpy(en_pos_embedding).float().unsqueeze(0), requires_grad=False)
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim)
+
+        self.apply(init_weights)
+
+    def forward(self, img: torch.FloatTensor) -> torch.FloatTensor:
+        x = self.to_patch_embedding(img)
+        x = x + self.en_pos_embedding
+        x = self.transformer(x)
+
+        return x
+
+
+class ViTDecoder(nn.Module):
+    def __init__(self, image_size: Union[Tuple[int, int], int], patch_size: Union[Tuple[int, int], int],
+                 dim: int, depth: int, heads: int, mlp_dim: int, channels: int = 3, dim_head: int = 64) -> None:
+        super().__init__()
+        image_height, image_width = image_size if isinstance(image_size, tuple) \
+                                    else (image_size, image_size)
+        patch_height, patch_width = patch_size if isinstance(patch_size, tuple) \
+                                    else (patch_size, patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        de_pos_embedding = get_2d_sincos_pos_embed(dim, (image_height // patch_height, image_width // patch_width))
+
+        self.num_patches = (image_height // patch_height) * (image_width // patch_width)
+        self.patch_dim = channels * patch_height * patch_width
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim)
+        self.de_pos_embedding = nn.Parameter(torch.from_numpy(de_pos_embedding).float().unsqueeze(0), requires_grad=False)
+        self.to_pixel = nn.Sequential(
+            Rearrange('b (h w) c -> b c h w', h=image_height // patch_height),
+            nn.ConvTranspose2d(dim, channels, kernel_size=patch_size, stride=patch_size)
+        )
+
+        self.apply(init_weights)
+
+    def forward(self, token: torch.FloatTensor) -> torch.FloatTensor:
+        x = token + self.de_pos_embedding
+        x = self.transformer(x)
+        x = self.to_pixel(x)
+
+        return x
+
+    def get_last_layer(self) -> nn.Parameter:
+        return self.to_pixel[-1].weight
+
+class Quantizer(nn.Module):
+    def __init__(self, config: ViTVQGANConfig):
+        super(Quantizer, self).__init__()
+        self.codebook = nn.Embedding(config.codebook_size, config.latent_dim)
+        self.codebook.weight.data.uniform_(-1.0 / config.codebook_size, 1.0 / config.codebook_size)
+    def forward(self, x):
+        x = torch.nn.functional.normalize(x, dim=-1)
+        embedding = torch.nn.functional.normalize(self.codebook.weight, dim=-1)
+        indices = torch.cdist(x, embedding).argmin(dim=-1)
+        quantized = self.codebook(indices)
+        codebook_loss = (quantized - x.detach()).pow(2).mean()
+        commitment_loss = 0.25 * (quantized.detach() - x).pow(2).mean()
+        quantize_loss = codebook_loss + commitment_loss
+        quantized = x + (quantized - x).detach() # copy gradients
+        return quantized, indices, quantize_loss
+
+class ViTVQGAN(nn.Module):
+    def __init__(self, config: ViTVQGANConfig):
+        super(ViTVQGAN, self).__init__()
+        self.config = config
+        self.encoder = ViTEncoder(config)
+        self.quant = Quantizer(config)
+        self.decoder = ViTDecoder(config)
+    def encode(self, z): return self.quant(self.encoder(z))[1]
+    def decode(self, z_quant): return self.decoder(z_quant)
+    def decode_indices(self, indices): return self.decoder(self.quant.codebook(indices))
+    def forward(self, x):
+        latent_embs = self.encoder(x)
+        quantized, indices, quantize_loss = self.quant(latent_embs)
+        image_recon = self.decoder(quantized)
+        return image_recon, indices, quantize_loss
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_size', type=int, default=128)
+    parser.add_argument('--patch_size', type=int, default=16)
+    parser.add_argument('--latent_tokens', type=int, default=256)
+    parser.add_argument('--codebook_size', type=int, default=2048)
+    parser.add_argument('--latent_dim', type=int, default=12)
+    parser.add_argument('--transformer', type=str, default='B')
+    parser.add_argument('--bs', type=int, default=32)
+    parser.add_argument('--mixed', type=bool, default=True)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--weight_decay', type=float, default=1e-4)
+    parser.add_argument('--warmup_steps', type=int, default=5000)
+    parser.add_argument('--train_steps', type=int, default=500000)
+    parser.add_argument('--dataset', type=str, default='imagenet')
+    parser.add_argument('--epochs', type=int, default=100000)
+    args = parser.parse_args()
+    args.min_lr = args.lr / 10.
+    config = ViTVQGANConfig(args.image_size, args.patch_size, args.codebook_size, args.latent_dim, args.transformer)
+
+    if args.dataset == 'imagenet':
+        project_name = 'vit-vqgan'
+        train_loader, _ = get_imagenet_loaders(args.image_size, args.bs)
+    # elif args.dataset == 'dmlab':
+    #     assert args.image_size == 64
+    #     project_name = 'titok-dmlab'
+    #     train_loader, _ = get_dmlab_image_loaders(args.bs)
+    # elif args.dataset == 'minecraft':
+    #     assert args.image_size == 128
+    #     project_name = 'titok-minecraft'
+    #     train_loader, _ = get_minecraft_image_loaders(args.bs)
+
+    run_name=f"{args.patch_size}px_{args.image_size}px_{args.transformer}_{args.latent_tokens}_{args.codebook_size}"
+
+    wandb.init(project=project_name, name=run_name, config=config.__dict__ | args.__dict__)
+
+    titok = ViTVQGAN(config).to(device)
+
+    optim = torch.optim.AdamW(titok.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    lr_sched = get_lr_scheduler(optim, args.warmup_steps, args.train_steps, args.min_lr)
+    scaler = GradScaler(enabled=args.mixed)
+
+    from perceptual_loss import PerceptualLoss
+    lpips_loss_fn = PerceptualLoss().to(device).eval()
+
+    print(f"STATS: enc_params={get_params_str(titok)}")
+
+    best_recon = float('inf')
+    for epoch in range(args.epochs):
+        bar = tqdm.tqdm(train_loader)
+        train_loss = 0.
+        codebook_usage = torch.zeros([config.codebook_size], device=device)
+        st = time.time()
+        for i, (images, _) in enumerate(bar):
+            images = images.to(device)
+            load_time = time.time() - st
+            optim.zero_grad()
+            with autocast("cuda", enabled=args.mixed):
+                image_recon, indices, quantize_loss = titok(images)
+                l1_loss = (image_recon - images).abs().mean()
+                perceptual_loss = lpips_loss_fn(image_recon, images).mean()
+                recon_loss = l1_loss + perceptual_loss
+                loss = recon_loss + quantize_loss
+            scaler.scale(loss).backward()
+            scaler.step(optim)
+            scaler.update()
+            nn.utils.clip_grad_norm_(titok.parameters(), max_norm=1.0)
+            lr_sched.step()
+            step_time = time.time() - st - load_time
+            codebook_usage[indices] = 1
+            if i % 500 == 0: 
+                codebook_usage_val = codebook_usage.sum().item() / config.codebook_size
+                wandb.log({"train/epoch": epoch, "train/loss": loss.item(), "train/recon_loss": recon_loss.item(), "train/quant_loss": quantize_loss.item(), "train/perceptual_loss": perceptual_loss.item(), "train/l1_loss": l1_loss.item(), "train/codebook_usage": codebook_usage_val, "benchmark/load_time": load_time, "benchmark/step_time": step_time, "train/lr": optim.param_groups[0]['lr']})
+                bar.set_description(f"e={epoch}: loss={loss.item():.3f} recon_loss={recon_loss.item():.3f} quant_loss={quantize_loss.item():.3f}")
+                if recon_loss.item() < best_recon:
+                    best_recon = recon_loss.item()
+                    torch.save({"config": config, "state_dict": titok.state_dict()}, f"titok_models/titok_{args.dataset}_{args.latent_tokens}_{args.codebook_size}.pt")
+            if i % 5000 == 0: 
+                images = [wandb.Image(img.permute(1, 2, 0).detach().cpu().numpy()) for img in images[:4]]
+                recons = [wandb.Image(img.permute(1, 2, 0).detach().cpu().numpy()) for img in image_recon[:4]]
+                codebook_usage *= 0
+                wandb.log({"images": images, "reconstructions": recons})
+            st = time.time()
+        train_loss /= len(train_loader)
+
