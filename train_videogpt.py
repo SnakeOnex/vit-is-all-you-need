3518e3a6575af41731d7de5473e7f259d70b9d52
Snakeonex <sheeproman@gmail.com>
2025-01-26 22:01:26 +0100

    projection bugfix + titok tokenizer

--- train_videogpt.py
+++ train_videogpt.py
@@ -42,6 +42,7 @@ class VideoGPT(nn.Module):
         self.tok_embed = nn.Embedding(config.codebook_size+1, config.n_embd)
         self.pos_embed = nn.Embedding(config.max_tokens, config.n_embd)
         self.transformer = Transformer(config.trans_config)
+        self.proj = nn.Linear(config.n_embd, config.codebook_size)
     def forward(self, x):
         B, T, N = x.shape
         sos = torch.zeros([B, 1], device=x.device, dtype=torch.long) + self.config.codebook_size
@@ -49,6 +50,7 @@ class VideoGPT(nn.Module):
         x = torch.cat([sos, y[:,:-1]], dim=-1)
         x = self.tok_embed(x) + self.pos_embed(torch.arange(T*N, device=x.device))
         logits = self.transformer(x)
+        logits = self.proj(logits)
         loss = F.cross_entropy(rearrange(logits, 'b s d -> (b s) d'), rearrange(y, 'b s -> (b s)'))
         return logits, loss
     def generate(self, tokens, n=1):
@@ -57,6 +59,7 @@ class VideoGPT(nn.Module):
             x = torch.cat([sos, tokens], dim=-1)
             x = self.tok_embed(x) + self.pos_embed(torch.arange(x.shape[1], device=x.device))
             logits = self.transformer(x)
+            logits = self.proj(logits)
             next_token = torch.argmax(logits[:, -1], dim=-1, keepdim=True)
             tokens = torch.cat([tokens, next_token], dim=-1)
         return tokens
@@ -67,11 +70,11 @@ class VideoGPT(nn.Module):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--frame_size', type=int, default=16)
-    parser.add_argument('--codebook_size', type=int, default=512)
-    parser.add_argument('--transformer', type=str, default='S')
-    parser.add_argument('--max_frames', type=int, default=4)
-    parser.add_argument('--condition_frames', type=int, default=4)
+    parser.add_argument('--frame_size', type=int, default=64)
+    parser.add_argument('--codebook_size', type=int, default=1024)
+    parser.add_argument('--transformer', type=str, default='B')
+    parser.add_argument('--max_frames', type=int, default=16)
+    parser.add_argument('--condition_frames', type=int, default=8)
     parser.add_argument('--dropout', type=float, default=0.0)
     parser.add_argument('--bs', type=int, default=32)
     parser.add_argument('--mixed', type=bool, default=True)
@@ -90,10 +93,12 @@ if __name__ == '__main__':
     run_name=f"{args.frame_size}_{args.transformer}_{args.codebook_size}_{args.max_frames}frames"
     if args.dataset == 'dmlab':
         train_loader, _ = get_dmlab_video_loaders(args.bs)
-        titok_params = torch.load(f"titok_models/titok_{args.dataset}_{args.frame_size}_{args.codebook_size}.pt")
-        titok = TiTok(titok_params["config"]).to(device).eval()
-        titok.load_state_dict(titok_params["state_dict"])
-        print(f"{titok_params['config']}")
+        # titok_params = torch.load(f"titok_models/titok_{args.dataset}_{args.frame_size}_{args.codebook_size}.pt")
+        # titok = TiTok(titok_params["config"]).to(device).eval()
+        # titok.load_state_dict(titok_params["state_dict"])
+        from test_import import get_titok_tokenizer
+        titok = get_titok_tokenizer()
+        titok = titok.to(device).eval()
 
     wandb.init(project=project_name, name=run_name, config=args.__dict__)
 
@@ -117,7 +122,8 @@ if __name__ == '__main__':
             B, T, C, H, W = videos.shape
 
             with torch.no_grad(): 
-                tokens = titok.encode(rearrange(videos, 'b t c h w -> (b t) c h w'))
+                _, tokens = titok.encode(rearrange(videos, 'b t c h w -> (b t) c h w'))
+                tokens = tokens["min_encoding_indices"].squeeze(1)
             tokens = rearrange(tokens, '(b t) n -> b t n', b=B)
 
             load_time = time.time() - st
@@ -128,19 +134,28 @@ if __name__ == '__main__':
             scaler.step(optim)
             scaler.update()
             lr_sched.step()
-            steps += 1
             step_time = time.time() - st - load_time
             if steps % 100 == 0: 
                 wandb.log({"train/loss": loss.item(), "benchmark/load_time": load_time, "benchmark/step_time": step_time, "train/lr": optim.param_groups[0]['lr'], "train/epoch": epoch, "train/steps": steps})
                 bar.set_description(f"e={epoch}: loss={loss.item():.3f}")
-            if steps % 2500 == 0: 
+            if steps % 1000 == 0: 
                 video_unrolled = rearrange(videos, 'b t c h w -> b h (t w) c')
                 wandb.log({"video": wandb.Image(video_unrolled[0].detach().cpu().numpy())})
                 with torch.no_grad(): 
                     gen_tokens = video_gpt.generate_frames(tokens[:,:args.condition_frames], n=args.max_frames - args.condition_frames)
-                    gen_video = titok.decode_indices(rearrange(gen_tokens, 'b (t n) -> (b t) n', t=T))
+                    # gen_video = titok.decode_indices(rearrange(gen_tokens, 'b (t n) -> (b t) n', t=T))
+                    print(f"{tokens.shape=}")
+                    recon_video = titok.decode_tokens(rearrange(tokens, 'b t n -> (b t) n', t=T))
+                    recon_video = torch.clamp(recon_video, 0.0, 1.0)
+                    recon_video = rearrange(recon_video, '(b t) c h w -> b t c h w', b=B)
+                    wandb.log({"recon_video": wandb.Image(rearrange(recon_video, 'b t c h w -> b h (t w) c')[0].detach().cpu().numpy())})
+
+                    gen_video = titok.decode_tokens(rearrange(gen_tokens, 'b (t n) -> (b t) n', t=T))
+                    gen_video = torch.clamp(gen_video, 0.0, 1.0)
+                    print(f"{gen_video.shape=} {gen_video.min()=} {gen_video.max()=}")
                     gen_video = rearrange(gen_video, '(b t) c h w -> b t c h w', b=B)
                     gen_video_unrolled = rearrange(gen_video, 'b t c h w -> b h (t w) c')
                     wandb.log({"gen_video": wandb.Image(gen_video_unrolled[0].detach().cpu().numpy())})
+            steps += 1
             st = time.time()
 
